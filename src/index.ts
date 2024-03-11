73e09077a8f0c427510ecf3a0f76e7cd5cb7fc3a
shreyas0924 <shreyasp0924@gmail.com>
2024-03-11 16:31:03 +0530

    prisma initialization

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const PORT = 3000;
+import "dotenv/config";
+
 app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello world 123");
 });
+
 app.listen(PORT, () => {
   console.log(`App running at port ${PORT}`);
 });
